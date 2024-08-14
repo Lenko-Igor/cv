03dc491913b00530c3bb74e8cc31c4df609ff4de
IgorLenko <mk2008@mail.ru>
2024-08-14 19:39:47 +0300

    feat: create new vertion

---
+++ constants/index.js
@@ -0,0 +1,16 @@
+export const LEVEL = {
+  BASIC: 'Basic',
+  PRE_INTERMEDIATE: 'Pre-Intermediate',
+  ADVANCED: 'Advanced',
+  EXPERT: 'Expert',
+  NATIVE: 'Native',
+}
+
+export const BLOCK_TITLES = {
+  CONTACTS: 'Contacts',
+  TECH_SKILLS: 'Technical skills',
+  ABOUT_MYSELF: 'Self presentation',
+  EXPERIENCE: 'Experience',
+  EDUCATION: 'Education',
+  LANGUAGES: 'Languages',
+}
