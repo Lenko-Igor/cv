62460840503847a984131b9b22bb87d1f28cf897
IgorLenko <mk2008@mail.ru>
2025-01-13 15:50:12 +0300

    create: add button

--- app.js
+++ app.js
@@ -3,20 +3,37 @@ import { BLOCK_TITLES } from './constants/index.js'
 
 const { createApp, ref } = Vue;
 
+let language = 'en';
+
 const app = createApp({
   data() {
     return {
-      titles: ref(BLOCK_TITLES),
-      user: ref(commonInfo),
-      techSkills: ref(techSkills),
-      selfPresentation: ref(selfPresentation),
-      experience: ref(experience),
-      languages: ref(languages),
-      educations: ref(educations),
+      titles: ref(BLOCK_TITLES[language]),
+      user: ref(commonInfo(language)),
+      techSkills: ref(techSkills(language)),
+      selfPresentation: ref(selfPresentation(language)),
+      experience: ref(experience(language)),
+      languages: ref(languages(language)),
+      educations: ref(educations(language)),
     };
   },
 
-  methods: {}
+  methods: {
+    selectLanguage() {
+        language = language === 'ru' ? 'en' : 'ru';
+
+        this.user = commonInfo(language);
+        this.titles = BLOCK_TITLES[language];
+        this.techSkills = techSkills(language);
+        this.languages = languages(language);
+        this.selfPresentation = selfPresentation(language);
+        this.experience = experience(language);
+        this.educations = educations(language);
+    }
+  },
+  mounted() {
+    document.getElementById("btn").addEventListener("click", this.selectLanguage)
+  }
 });
 
 app.mount("#app");
--- constants/index.js
+++ constants/index.js
@@ -1,17 +1,182 @@
+export const LANGUAGE = {
+    EN: 'en',
+    RU: 'ru',
+}
+
 export const LEVEL = {
-  BASIC: 'Basic',
-  PRE_INTERMEDIATE: 'Pre-Intermediate (A2)',
-  INTERMEDIATE: 'Intermediate (B1)',
-  ADVANCED: 'Advanced',
-  EXPERT: 'Expert',
-  NATIVE: 'Native',
+    [LANGUAGE.EN]: {
+        BASIC: 'Basic',
+        PRE_INTERMEDIATE: 'Pre-Intermediate (A2)',
+        INTERMEDIATE: 'Intermediate (B1)',
+        ADVANCED: 'Advanced',
+        EXPERT: 'Expert',
+        NATIVE: 'Native',
+    },
+    [LANGUAGE.RU]: {
+        BASIC: 'Базовый',
+        PRE_INTERMEDIATE: 'Предпрофессиональный (A2)',
+        INTERMEDIATE: 'Профессиональный (B1)',
+        ADVANCED: 'Высокий',
+        EXPERT: 'Эксперт',
+        NATIVE: 'Родной',
+    },
 }
 
 export const BLOCK_TITLES = {
-  CONTACTS: 'Contacts',
-  TECH_SKILLS: 'Technical skills',
-  ABOUT_MYSELF: 'Self presentation',
-  EXPERIENCE: 'Experience',
-  EDUCATION: 'Education',
-  LANGUAGES: 'Languages',
+    [LANGUAGE.EN]: {
+        CONTACTS: 'Contacts',
+        TECH_SKILLS: 'Technical skills',
+        ABOUT_MYSELF: 'Self presentation',
+        EXPERIENCE: 'Experience',
+        EDUCATION: 'Education',
+        LANGUAGES: 'Languages',
+    },
+    [LANGUAGE.RU]: {
+        CONTACTS: 'Контакты',
+        TECH_SKILLS: 'Технические навыки',
+        ABOUT_MYSELF: 'Обо мне',
+        EXPERIENCE: 'Опыт работы',
+        EDUCATION: 'Образование',
+        LANGUAGES: 'Языки',
+    },
+}
+
+export const TITLES = {
+    [LANGUAGE.EN]: {
+        PHONE: 'Phone',
+        EMAIL: 'E-mail',
+        LINKDIN: 'LinkdIn',
+        PROGRAMMING_LANGUAGES: 'Programming languages:',
+        DEV_TECHNOLOGIES: 'Development technologies',
+        DATABASES: 'Databases',
+        IDE: 'IDE/Development tools',
+        VCS: 'Issue Tracking/VCS',
+        DEV_METHODOLOGIES: 'Development methodologies',
+
+    },
+    [LANGUAGE.RU]: {
+        PHONE: 'Телефон',
+        EMAIL: 'Эл.почта',
+        LINKDIN: 'LinkdIn',
+        PROGRAMMING_LANGUAGES: 'Языки программирования:',
+        DEV_TECHNOLOGIES: 'Технологии разработки',
+        DATABASES: 'Базы данных',
+        IDE: 'IDE/Инструменты разработки',
+        VCS: 'Системы управления версиями/VCS',
+        DEV_METHODOLOGIES: 'Методологии разработки',
+    },
+}
+
+export const USER = {
+    [LANGUAGE.EN]: {
+        FIRST_NAME: 'Igor',
+        LAST_NAME: 'Lenko',
+        SELF_PRESENTATION: `I'm Frontend developer with experience in design and development of frontend applications using React.
+                            Also have experience in onboarding and teaching new coworkers on projects.
+                            I've worked as with big teams such  with small on different projects and today I have a good background with projects from different domains.
+                            There are like economic, social, medical, sports and etc. 
+                            Always open to new challenging tasks which require fast learning and finding the most efficient solution.
+                            `
+    },
+    [LANGUAGE.RU]: {
+        FIRST_NAME: 'Игорь',
+        LAST_NAME: 'Ленько',
+        SELF_PRESENTATION: `Я React фронтенд разработчик с опытом проектирования фронтенд-приложений.
+                            Работал над проектами из разных сфер деятельности такие как, экономика, социальная сфера, медицина, спорт и т. д.
+                            Есть опыт в адаптации и обучении новых коллег на проектах.
+                            Всегда открыт для новых сложных задач, требующих быстрого обучения и поиска наиболее эффективного решения.
+                            `  
+    }
+}
+
+export const EXPERIENCES = {
+    [LANGUAGE.EN]: {
+        SOFTTECO: {
+            period: 'August 2021 - till now',
+            jobTitle: 'Frontend Developer',
+            responsibilities: [
+                'Develop and launch some projects',
+                'Code-review',
+                'Estimate some tasks',
+                'Selection of technologies for projects',
+                'Communicate with customers',
+                'Communicate with team',
+                'Onboard new developers on projects',
+                'etc.'
+            ],
+        },
+        FREELANCER: {
+            period: 'January 2020 - August 2021',
+            jobTitle: 'Frontend Developer (HTML, CSS, JavaScript)',
+            responsibilities: [
+                'Support some projects',
+                'Fix bugs',
+                'Develop landing pages',
+                'Create some extensions',
+            ]
+        },
+    },
+    [LANGUAGE.RU]: {
+        SOFTTECO: {
+            period: 'Август 2021 - по настоящее время',
+            jobTitle: 'Frontend разработчик',
+            responsibilities: [
+                'Разработка и запуск некоторых проектов',
+                'Обзор кода',
+                'Оценка некоторых задач',
+                'Выбор технологий для проектов',
+                'Общение с заказчиками',
+                'Общение с командой',
+                'Приветствие новых разработчиков на проектах',
+                'и т. д.'
+            ],
+        },
+        FREELANCER: {
+            period: 'Январь 2020 - Август 2021',
+            jobTitle: 'Frontend разработчик (HTML, CSS, JavaScript)',
+            responsibilities: [
+                'Поддержка некоторых проектов',
+                'Исправление багов',
+                'Разработка лендингов',
+                'Создание расширений',    
+            ],
+        },
+    }
+}
+
+export const EDUCATIONS = {
+    [LANGUAGE.EN]: {
+        0: {
+            name: 'RSSchool',    
+            subject: 'Frontend developer JavaScript',
+            period: 'December 2020 - May 2021',
+        },
+        1: {
+            name: 'IT-Academy',
+            subject: 'Development web-applications on JavaScript',
+            period: 'July 2020 - November 2020',
+        },
+        2: {
+            name: 'IT-Academy',
+            subject: 'Development web-applications on HTML,CSS and JavaScript',
+            period: 'February 2020 - June 2020',
+        },
+    },
+    [LANGUAGE.RU]: {
+        0: {
+            name: 'RSSchool',    
+            subject: 'Frontend разработчик JavaScript',
+            period: 'Декабрь 2020 - Май 2021',
+        },
+        1: {
+            name: 'IT-Academy',
+            subject: 'Разработка веб-приложений на JavaScript',
+            period: 'Июль 2020 - Ноябрь 2020',
+        },
+        2: {
+            name: 'IT-Academy',
+            subject: 'Разработка веб-приложений на HTML,CSS и JavaScript',
+            period: 'Февраль 2020 - Июнь 2020',
+        },
+    }
 }
